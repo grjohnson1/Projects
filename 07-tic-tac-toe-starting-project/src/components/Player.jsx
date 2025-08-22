8f1723199ab5af3e0d84562313553f6c0f148375
grjohnson1 <grjohnson@gmail.com>
2025-08-22 13:05:46 -0600

    Edit Player Name via input

--- 07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,22 +1,27 @@
 import { useState } from "react";
 
-export default function Player({name, symbol}) {
+export default function Player({initialName, symbol}) {
+    const [playerName, setPlayerName] = useState(initialName);
     const [isEditing, setIsEditing] = useState(false);
 
     function handleEditClick() {
-        setIsEditing(!isEditing);
+        setIsEditing((editing) => !editing); // best practice route
+        // setIsEditing(!isEditing) - waits to execute which could mean state isn't updating as expected
     }
 
-    let playerName = <span className="player-name">{name}</span>;
+    function handleChange(event) {
+        setPlayerName(event.target.value);
+    }
 
+    let editablePlayerName = <span className="player-name">{playerName}</span>;
     if (isEditing) {
-        playerName = <input type="text" required placeholder="enter name" />;
+        editablePlayerName = <input type="text" required placeholder="enter name" value={playerName} onChange={handleChange} />;
     }
 
     return (
         <li>
             <span className="player">
-                {playerName}
+                {editablePlayerName}
                 <span className="player-symbol">{symbol}</span>
             </span>
             <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
