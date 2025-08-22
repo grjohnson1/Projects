21b81ed6aba4ed666675032ce9dd21068dbd034a
grjohnson1 <grjohnson@gmail.com>
2025-08-22 14:41:25 -0600

    create copy of array so that changes do not update immediately (Immutable)

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,9 +1,7 @@
-
 import Player from './components/Player'
+import GameBoard from './components/GameBoard';
 
 function App() {
-  
-
   return (
     <main>
       <div id="game-container">
@@ -11,7 +9,7 @@ function App() {
           <Player initialName="Player 1" symbol="X" />
           <Player initialName="Player 2" symbol="O" />
         </ol>
-        GAME BOARD
+        <GameBoard />
       </div>
     </main>
   )
---
+++ 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -0,0 +1,38 @@
+import {useState} from 'react';
+
+const initialGameBoard = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null]
+];
+
+export default function GameBoard() {
+    const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+    function handleSelectSquare(rowIndex, colIndex) {
+        setGameBoard((prevGameBoard) => {
+            // create and change a copy of array otherwise it updates immediately
+            // see https://academind.com/tutorials/reference-vs-primitive-values
+
+            const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+            updatedBoard[rowIndex][colIndex] = "X";
+            return updatedBoard;
+        });
+    }
+
+    return (
+        <ol id="game-board">
+            {gameBoard.map((row, rowIndex) => (
+                <li key={rowIndex}>
+                    <ol>
+                        {row.map((playerSymbol, colIndex) => (
+                            <li key={colIndex}>
+                                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                            </li>
+                        ))}
+                    </ol>
+                </li>
+            ))}
+        </ol>
+    )
+}
