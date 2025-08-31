ad7f7eca9dd7de7d7758b2c0e1c879a3ed64bd29
grjohnson1 <grjohnson@gmail.com>
2025-08-31 10:51:50 -0600

    Use direct DOM updates only if slims code down but be very careful as it should go through React

--- Final-Countdown-Game/src/components/Learning.jsx
+++ Final-Countdown-Game/src/components/Learning.jsx
@@ -4,6 +4,7 @@ export default function Learning() {
             <ol>
                 <li>React Ref will allow access to the DOM element completely. Slimming down the code.</li>
                 <li>Ref name.current. is the way to gain access to everything the DOM has access too. Compare build 2 to 3 for example.</li>
+                <li>Changes should happen in React for DOM changes and not directly to DOM. So be careful.</li>
             </ol>
         </section>
     )
