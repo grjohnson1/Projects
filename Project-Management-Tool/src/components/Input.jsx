0150f09ca9c5832ea115128d1be196e9b81c6e42
grjohnson1 <grjohnson@gmail.com>
2025-09-02 12:19:23 -0600

    Adding Project without validation to console.log

--- Project-Management-Tool/src/components/Input.jsx
+++ Project-Management-Tool/src/components/Input.jsx
@@ -1,8 +1,18 @@
-export default function Input({ label, textArea, ...props }) {
+// older version uses import {forwardRef} from 'react'
+
+// older version would use forwardRef
+//const Input = forwardRef(function Input({ label, textArea, ...props }, ref) {
+export default function Input({ label, textArea, ref, ...props }) {
     const classes = "w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600";
 
     return <p className="flex flex-col gap-1 my-4">
                 <label className="text-sm font-bold uppercase text-stone-500">{label}</label>
-                {textArea ? <textarea className={classes} {...props} /> : <input className={classes}  {...props} />}
+                {
+                    textArea ? 
+                        <textarea ref={ref} className={classes} {...props} /> : 
+                        <input ref={ref} className={classes}  {...props} />
+                }
             </p>
 }
+// older version
+//export default Input;
