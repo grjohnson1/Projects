d37bb089bc06392799f2f8560fc8a90fcc3aed08
grjohnson1 <grjohnson@gmail.com>
2026-01-26 16:12:42 -0700

    open stored user places

--- Place-Picker/src/http.js
+++ Place-Picker/src/http.js
@@ -9,6 +9,17 @@ export async function fetchAvailablePlaces() {
     return responseData.places;
 }
 
+export async function fetchUserPlaces() {
+    const response = await fetch('http://localhost:3000/user-places');
+    const responseData = await response.json();
+
+    if (!response.ok) { // 200 or 300 response is ok
+        throw new Error('Failed to fetch user places');
+    }
+
+    return responseData.places;
+}
+
 export async function updateUserPlaces(places) {
     const response = await fetch('http://localhost:3000/user-places', {
         method: 'PUT',
