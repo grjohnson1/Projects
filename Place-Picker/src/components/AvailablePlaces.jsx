9926b876811e8ca00f5d66d0ee186f0869ce8312
grjohnson1 <grjohnson@gmail.com>
2026-01-26 15:22:37 -0700

    adding user data to backend data location

--- Place-Picker/src/components/AvailablePlaces.jsx
+++ Place-Picker/src/components/AvailablePlaces.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react';
 import Places from './Places.jsx';
 import ErrorPage from './Error.jsx';
 import { sortPlacesByDistance } from '../loc.js';
+import { fetchAvailablePlaces } from '../http.js';
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
@@ -14,16 +15,11 @@ export default function AvailablePlaces({ onSelectPlace }) {
       setIsFetching(true);
 
       try {
-        const response = await fetch('http://localhost:3000/places');
-        const responseData = await response.json();
-
-        if (!response.ok) { // 200 or 300 response is ok
-          throw new Error('Failed to fetch places');
-        }
+        const places = await fetchAvailablePlaces();
 
         navigator.geolocation.getCurrentPosition(
           (position) => {
-            const sortedPlaces = sortPlacesByDistance(responseData.places, position.coords.latitude, position.coords.longitude);
+            const sortedPlaces = sortPlacesByDistance(places, position.coords.latitude, position.coords.longitude);
 
             setAvailablePlaces(sortedPlaces);
             setIsFetching(false);
---
+++ Place-Picker/src/http.js
@@ -0,0 +1,28 @@
+export async function fetchAvailablePlaces() {
+    const response = await fetch('http://localhost:3000/places');
+    const responseData = await response.json();
+
+    if (!response.ok) { // 200 or 300 response is ok
+        throw new Error('Failed to fetch places');
+    }
+
+    return responseData.places;
+}
+
+export async function updateUserPlaces(places) {
+    const response = await fetch('http://localhost:3000/user-places', {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({places})
+    });
+
+    const responseData = await response.json();
+
+    if (!response.ok) {
+        throw new Error('Failed to update user places');
+    }
+
+    return responseData.message;
+}
