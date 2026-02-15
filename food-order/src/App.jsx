614109f92384a3401102ed4b4b9e313083926d7c
grjohnson1 <grjohnson@gmail.com>
2026-02-15 16:34:16 -0700

    adding and removing items from cart

--- food-order/src/App.jsx
+++ food-order/src/App.jsx
@@ -1,16 +1,17 @@
 import Header from "./components/Header";
 import Meals from "./components/Meals";
+import { CartContextProvider } from "./Store/CartContext";
 import Cart from "./components/cart";
 import CheckoutForm from "./components/CheckoutForm";
 
 function App() {
   return (
-    <>
+    <CartContextProvider>
       <Header />
       <Meals />
       <Cart />
       <CheckoutForm />
-    </>
+    </CartContextProvider>
   );
 }
 
--- food-order/src/components/Header.jsx
+++ food-order/src/components/Header.jsx
@@ -1,14 +1,22 @@
+import {useContext } from 'react';
+
 import imgLogo from '../assets/logo.jpg';
 import Button from "./UI/Button";
+import CartContext from "../Store/CartContext";
 
 export default function Header() {
+    const cartContext = useContext(CartContext);
+    const totalCartItems = cartContext.items.reduce((totalNumberOfItems, item) => {
+        return totalNumberOfItems + item.quantity;
+    }, 0);
+
     return <header id="main-header">
         <div id="title">
             <img src={imgLogo} alt="Delicious Food Logo" />
             <h1>Delicious Food</h1>
         </div>
         <nav>
-            <Button textOnly >Cart (0)</Button>
+            <Button textOnly >Cart ({totalCartItems})</Button>
         </nav>
     </header>;
 }
